6c029bd4954d65a7127c51f51a9e8af1cdf7923c
Willian de Paula <willian.paula@f1rst.com.br>
2024-11-05 11:28:13 -0300

    instanciando classe endereco

--- api/src/main/java/med/voll/api/endereco/Endereco.java
+++ api/src/main/java/med/voll/api/endereco/Endereco.java
@@ -1,5 +1,16 @@
 package med.voll.api.endereco;
 
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Endereco {
     private String logradouro;
     private String bairro;
@@ -9,4 +20,13 @@ public class Endereco {
     private String cidade;
     private String uf;
 
+    public Endereco(DadosEndereco dados) {
+        this.logradouro = dados.logradouro();
+        this.bairro = dados.bairro();
+        this.cep = dados.cep();
+        this.numero = dados.numero();
+        this.complemento = dados.complemento();
+        this.cidade = dados.cidade();
+        this.uf = dados.uf();
+    }
 }
